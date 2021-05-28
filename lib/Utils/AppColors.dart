ff27c2c8e0c6aaf3712652646cb91e81461b5f0c
behrooz azimifar <behrooz2546@gmail.com>
2021-05-28 20:02:30 +0430

    some refactor

---
+++ lib/Utils/AppColors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundColor = const Color.fromRGBO(242, 245, 248, 1);
+  static const Color black = const Color.fromRGBO(0, 16, 55, 1);
+  static const Color lineViewColor = const Color.fromRGBO(234, 234, 244, 1);
+  static const Color gray = const Color.fromRGBO(125, 136, 155, 1);
+  static const Color main = const Color.fromRGBO(46, 35, 101, 1);
+}
---
+++ lib/Utils/AppStyles.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:trytulu/Utils/AppColors.dart';
+
+class AppStyles {
+  static const appbar_title = TextStyle(
+    fontFamily: 'Montserrat',
+    color: AppColors.black,
+    fontSize: 20,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const card_view_body = TextStyle(
+    fontFamily: 'Montserrat',
+    color: AppColors.gray,
+    fontSize: 14,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const checklist_item_title = TextStyle(
+    fontFamily: 'Montserrat',
+    color: AppColors.main,
+    fontSize: 16,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700,
+  );
+}
